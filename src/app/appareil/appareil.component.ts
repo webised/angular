6e1443cd4414c6b878126833db703a5bca971b43
sessinou <gbenounicolas@gmail.com>
2018-11-05 22:48:54 +0100

    test

---
+++ src/app/appareil/appareil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appareil',
+  templateUrl: './appareil.component.html',
+  styleUrls: ['./appareil.component.sass']
+})
+export class AppareilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
